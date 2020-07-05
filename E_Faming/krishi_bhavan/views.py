6a838cf5b77e2e86b33bf1c4ca0a1dab1141c64a
martinbenny1090 <martin8086benny@gmail.com>
2020-07-05 07:50:06 +0530

    asset added

--- E_Faming/krishi_bhavan/views.py
+++ E_Faming/krishi_bhavan/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, View 
+from django.http import HttpResponse
 
 # Create your views here.
+
+def HomeView(request):
+    return render(request, "E-Faming.html")
+    
